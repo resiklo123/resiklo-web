3dbed2c4444c3474fa113e71733978c336323e51
resiklo123 <resikloemail>
2025-04-26 11:57:14 +0800

    Added Products page

--- src/app/products1/page.tsx
+++ src/app/products1/page.tsx
@@ -25,16 +25,16 @@ export default function ProductsPage() {
 
       {/* Grid Section */}
       <section className="py-20 bg-gray-100">
-        <div className="max-w-6xl mx-auto">
+        <div className="w-full px-8 sm:px-16 lg:px-24">
           <h2 className="text-4xl font-bold text-center mb-12">Major Categories</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-10">
 
             {/* Category 1 */}
             <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition">
               <Image
                 src={shredderCrusher}
                 alt="Shredders and Crushers"
-                width={300}
+                width={200}
                 height={200}
                 className="object-contain rounded-lg mb-6"
               />
@@ -70,7 +70,7 @@ export default function ProductsPage() {
               <Image
                 src={servicesMolds}
                 alt="Services and Molds"
-                width={300}
+                width={200}
                 height={200}
                 className="object-contain rounded-lg mb-6"
               />
