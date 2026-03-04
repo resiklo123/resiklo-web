394b0895dde77938da3021f0d6e481f4163c0fc8
resiklo123 <resikloemail>
2026-03-05 07:41:05 +0800

    Fixed package.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Roboto } from "next/font/google"
 import "./globals.css"
 import { Nav } from "./nav"
 import { Footer } from "./footer"
+import logo from "./images/Resiklo-Logo.png"
 
 const roboto = Roboto({
   weight: "400",
@@ -14,6 +15,21 @@ export const metadata: Metadata = {
   description: "Resiklo Machine Shop",
 }
 
+const siteUrl = "https://resiklo.org"
+
+const organizationSchema = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  name: "Resiklo Machine Shop",
+  url: siteUrl,
+  logo: new URL(logo.src, siteUrl).toString(),
+  sameAs: [
+    "https://www.facebook.com/resiklomachines",
+    "https://www.youtube.com/channel/UCqI-N5TRrWk3qHShL0Tyv0Q",
+    "https://www.instagram.com/resiklo.mfg/",
+  ],
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -21,6 +37,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
+        />
+      </head>
       <body className={`${roboto.className} antialiased`}>
         <Nav />
         <div className="flex flex-col min-h-screen">
